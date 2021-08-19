9b6582b1e2ad588539a7ab42d2a73f26e1145072
Franco Benitez <francobenitez980@gmail.com>
2021-08-19 14:45:58 -0300

    hbl terminado

---
+++ tarea/hbl/src/Contenedor.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+
+class Contenedor {
+  constructor(nameFile) {
+    this.nameFile = nameFile;
+  }
+
+  async save(data) {
+    try {
+      let contenido = await fs.promises.readFile(this.nameFile, "utf-8");
+      data.id = 1;
+      if (contenido != "") {
+        contenido = JSON.parse(contenido);
+        data.id = contenido[contenido.length - 1].id + 1;
+      }
+      let array = [
+        ...contenido,
+        {
+          name: data.name,
+          price: data.price,
+          thumbnail: data.photo,
+          id: data.id,
+        },
+      ];
+      await fs.promises.writeFile(
+        this.nameFile,
+        JSON.stringify(array, null, 2)
+      );
+      console.log(`subido el producto ${data.title}`);
+      return data.id;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async getAll() {
+    try {
+      let content = await fs.promises.readFile(this.nameFile, "utf-8");
+      if (content == "") return [];
+      return JSON.parse(content);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+}
+
+module.exports = Contenedor;
---
+++ tarea/hbl/src/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const handlebars = require("express-handlebars");
+const Contenedor = require("./Contenedor");
+const contenedor = new Contenedor("./src/productos.json");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+    defaultLayout: "index.hbs",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
+  })
+);
+
+app.set("views", "./src/views");
+app.set("view engine", "hbs");
+app.use("/", express.static("../public"));
+
+app.get("/", (req, res) => {
+  res.render("nuevoProducto");
+});
+
+app.get("/productos", async (req, res) => {
+  const productos = await contenedor.getAll();
+  res.render("productos", {
+    productos,
+  });
+});
+
+app.post("/productos", async (req, res) => {
+  const { body } = req;
+  try {
+      await contenedor.save(body);
+      res.redirect("/");
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      error: error.message,
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`);
+});
