492f8373ef20db34dad2df2a50a9a0c1b5624b50
Franco Benitez <francobenitez980@gmail.com>
2021-08-20 10:52:21 -0300

    terminado ejs

---
+++ tarea/ejs/src/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const Contenedor = require("./Contenedor");
+const contenedor = new Contenedor("./src/productos.json");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("pages/nuevoProducto", {
+    solapaNombre: "Nuevo producto",
+  });
+});
+
+app.get("/productos", async (req, res) => {
+  const productos = await contenedor.getAll();
+  res.render("pages/productos", {
+    productos,
+    solapaNombre: "Listado de productos",
+  });
+});
+
+app.post("/productos", async (req, res) => {
+  const { body } = req;
+  try {
+    await contenedor.save(body);
+    res.redirect("/");
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      error: error.message,
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`);
+});
